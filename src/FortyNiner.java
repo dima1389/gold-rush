064ce1779cdca46cd1d93440b2bb288612ab8a81
Dima <dimitrije.cuk@gmail.com>
2026-05-11 22:24:15 +0200

    Refactor FortyNiner class to add getter and setter methods for endurance, money, tools, and rnd

--- src/FortyNiner.java
+++ src/FortyNiner.java
@@ -13,4 +13,38 @@ public class FortyNiner {
         this.tools = new ArrayList<>(tools);
         this.rnd = new Random();
     }
+
+    public int getEndurance() {
+        return endurance;
+    }
+
+    public void setEndurance(int endurance) {
+        this.endurance = endurance;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public ArrayList<Tool> getTools() {
+        return new ArrayList<>(tools);
+    }
+
+    public void setTools(ArrayList<Tool> tools) {
+        this.tools = new ArrayList<>(tools);
+    }
+
+    public Random getRnd() {
+        return rnd;
+    }
+
+    public void setRnd(Random rnd) {
+        if (rnd != null) {
+            this.rnd = rnd;
+        }
+    }
 }
