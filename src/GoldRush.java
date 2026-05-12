12f3e9b039f93faa2ebbb915078cd876f1463195
Dima <dimitrije.cuk@gmail.com>
2026-05-12 14:48:45 +0200

    Refactor survive() method to include week tracking and output for endurance and money

--- src/GoldRush.java
+++ src/GoldRush.java
@@ -11,6 +11,19 @@ public class GoldRush {
     public void survive() {
         fortyNiner = new FortyNiner();
         currentWeek = 1;
+
+        while (currentWeek <= 20) {
+            System.out.printf(
+                "End of week %d: endurance %d%%, money $%d.%n",
+                currentWeek,
+                fortyNiner.getEndurance(),
+                fortyNiner.getMoney()
+            );
+
+            currentWeek++;
+        }
+
+        System.out.printf("%nAll 20 weeks are over. Final money: $%d.%n", fortyNiner.getMoney());
     }
 
     public void loadGame() {
